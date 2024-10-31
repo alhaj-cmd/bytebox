287e33a918f4d70649527fe5044e2ebf1a3fe65c
alhaj-cmd <alhazsarker5523@gmail.com>
2024-10-31 22:24:56 +0600

    daisy ui use

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-      
+      <button className="btn btn-active btn-secondary">Secondary</button>
       </div>
       <p className="text-3xl bg-red-400">
         Click on the Vite and React logos to learn more
