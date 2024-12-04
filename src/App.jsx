ec9e89c2967016007441741b35fa01591fd908c0
alhaj-cmd <alhazsarker5523@gmail.com>
2024-12-05 02:03:52 +0600

    check file

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+import Nav from './assets/Home/Nav/Nav'
 import './App.css'
 
 function App() {
@@ -8,9 +8,7 @@ function App() {
       <div>
       <button className="btn btn-active btn-secondary">Secondary</button>
       </div>
-      <p className="text-3xl bg-red-400">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Nav></Nav>
     </>
   )
 }
---
+++ src/assets/Home/Nav/Nav.jsx
@@ -0,0 +1,18 @@
+function Nav() {
+
+    return (
+      <>
+       <div>
+       <div>
+        <h1>Personal <span className="text-3xl text-red-300">Blogs</span></h1>
+        {/* <button className="btn btn-active btn-secondary">Secondary</button> */}
+        </div>
+        <ul>
+            <li>Home</li>
+        </ul>
+       </div>
+      </>
+    )
+  }
+  
+  export default Nav
