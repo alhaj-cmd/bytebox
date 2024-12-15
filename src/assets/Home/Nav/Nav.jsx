a9d22dd18dfbacd421420530f0ae0303e16deb83
alhaj-cmd <alhazsarker5523@gmail.com>
2024-12-16 00:50:06 +0600

    file setup

--- src/assets/Home/Nav/Nav.jsx
+++ src/assets/Home/Nav/Nav.jsx
@@ -2,18 +2,18 @@ function Nav() {
 
     return (
       <>
-       <div>
-       <div>
-        <h1>Personal <span className="text-3xl text-red-300">The Blogs </span></h1>
-        {/* <button className="btn btn-active btn-secondary">Secondary</button> */}
-        </div>
-        <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contacts</li>
-            <li>Gellary</li>
-        </ul>
-       </div>
+      <div className="navbar bg-base-100 shadow-lg">
+  <div className="flex-1">
+    <a className="btn btn-ghost text-xl">Book <span>Shop</span></a>
+  </div>  
+  <div className="flex-none">
+    <ul className="menu menu-horizontal px-1">
+      <li><a>Home</a></li>
+      <li><a>About</a></li>
+      <li><a>Blogs</a></li>
+    </ul>
+  </div>
+</div>
       </>
     )
   }
