ffe7819e00875ec914beba5c5e2ef7014f37c23c
alhaj-cmd <alhazsarkar5523@gmail.com>
2024-12-26 01:02:17 +0600

    routhing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
 // import { StrictMode } from 'react'
-import { ReactDOM } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <>
-    <App />
-  </>,
-)
+// createRoot(document.getElementById('root')).render(
+//   <>
+//     <App />
+//   </>,
+// )
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Home from "./assets/pages/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
