8a01458a3226c473608be4bc9ac69aa40ad303c3
alhaj-cmd <alhazsarkar5523@gmail.com>
2025-01-19 00:01:21 +0600

    navigation section select

---
+++ src/assets/component/BookMark/Bookmark.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Bookmark = () => {
+    return (
+        <div>
+            <h3>This is bookmarks section</h3>
+        </div>
+    );
+};
+
+export default Bookmark;
