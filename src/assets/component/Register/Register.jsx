c87a2fabf241228791273a8f2971d504bb3faf81
alhaj-cmd <alhazsarkar5523@gmail.com>
2025-01-20 23:36:00 +0600

    Register section included

--- src/assets/component/Register/Register.jsx
+++ src/assets/component/Register/Register.jsx
@@ -2,8 +2,26 @@ import React from 'react';
 
 const Register = () => {
     return (
-        <div>
+        <div className='text-center'>
             <h2>I'm a Register section</h2>
+            <form action="">
+                <div>
+                    <p>Nama</p>
+                    <input type="text" className='input input-bordered w-full max-w-xs' />
+                </div>
+                <div>
+                    <p>Nama</p>
+                    <input type="text" className='input input-bordered w-full max-w-xs' />
+                </div>
+                <div>
+                    <p>Nama</p>
+                    <input type="text" className='input input-bordered w-full max-w-xs' />
+                </div>
+                <div>
+                    <p>Nama</p>
+                    <input type="text" className='input input-bordered w-full max-w-xs' />
+                </div>
+            </form>
         </div>
     );
 };
