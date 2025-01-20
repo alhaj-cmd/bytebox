22351e5931b1242d580fb3663b3c935cc6feafeb
alhaj-cmd <alhazsarkar5523@gmail.com>
2025-01-20 23:38:02 +0600

    input filled added

--- src/assets/component/Register/Register.jsx
+++ src/assets/component/Register/Register.jsx
@@ -21,6 +21,7 @@ const Register = () => {
                     <p>Nama</p>
                     <input type="text" className='input input-bordered w-full max-w-xs' />
                 </div>
+                <button className='btn btn-primary'>Button</button>
             </form>
         </div>
     );
