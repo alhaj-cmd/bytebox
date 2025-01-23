4095b33ffc41a2f31376524b328fb5629ea36f02
alhaj-cmd <alhazsarkar5523@gmail.com>
2025-01-23 22:58:20 +0600

    login section add

--- src/assets/component/Login/Login.jsx
+++ src/assets/component/Login/Login.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
 
 const Login = () => {
+
+    const handleLogin = (e) =>{
+        e.preventDefault()
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const confirmpassword = e.target.confirmpassword.value;
+        console.log(name, email, confirmpassword);
+    }
+
     return (
         <div>
-            <h2>This pages is login</h2>
+             <div className='text-center'>
+            <h2>I'm a LogIN section</h2>
+            <form onSubmit={handleLogin} action="">
+                <div>
+                    <p>Nama</p>
+                    <input name='name' type="text" className='input input-bordered w-full max-w-xs' />
+                </div>
+                <div>
+                    <p>Email</p>
+                    <input name='email' type="text" className='input input-bordered w-full max-w-xs' />
+                </div>
+                <div>
+                    <p>Confirm Password</p>
+                    <input name='confirmpassword' type="text" className='input input-bordered w-full max-w-xs' />
+                </div>
+                <button type='submit' className='btn btn-primary'>Button</button>
+            </form>
+        </div>
         </div>
     );
 };
