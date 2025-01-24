3f0ccf8b171707b9cdcf7b9868d37b6d4c307b0e
alhaj-cmd <alhazsarkar5523@gmail.com>
2025-01-24 22:55:04 +0600

    register section add

--- src/assets/component/AuthProvider/AuthProvider.jsx
+++ src/assets/component/AuthProvider/AuthProvider.jsx
@@ -1,8 +1,15 @@
 import React, { createContext, useState } from 'react';
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
 export const AuthContext = createContext(null);
 
 const AuthProvider = ({children}) => {
+
     const [user, setName]= useState('tmi kno bujona');
+    const registerUser = (email, password) =>{
+        createUserWithEmailAndPassword(auth, email, password)
+
+    }
    
     
     return (
