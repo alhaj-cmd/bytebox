f8fa461045ebf171eb0367d31799efd90620c6a8
alhaj-cmd <alhazsarkar5523@gmail.com>
2025-01-25 23:29:19 +0600

    authentication

--- src/assets/component/AuthProvider/AuthProvider.jsx
+++ src/assets/component/AuthProvider/AuthProvider.jsx
@@ -1,20 +1,24 @@
 import React, { createContext, useState } from 'react';
 import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from '../../../Firebase/Firebase-init';
 
 export const AuthContext = createContext(null);
 
 const AuthProvider = ({children}) => {
 
-    const [user, setName]= useState('tmi kno bujona');
+    // const [user, setName]= useState('tmi kno bujona');
     const registerUser = (email, password) =>{
         createUserWithEmailAndPassword(auth, email, password)
 
     }
    
+    const authInfo ={
+        registerUser
+    }
     
     return (
         <div>
-            <AuthContext.Provider value={user}>{children}</AuthContext.Provider>
+            <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
         </div>
     );
 };
